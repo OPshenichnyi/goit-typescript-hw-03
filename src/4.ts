ba3457a4f8e5846cf6527d8722d597cb0847d28e
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2023-10-28 10:56:34 +0300

    add files

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,27 @@
+class Key {
+    private signature: number = Math.random();
+    getSignature(): number{
+        return this.signature
+    }
+}
+
+class Person {
+    constructor(private key: Key) { }
+    getKey(): Key{
+        return this.key
+    }
+}
+
+abstract class House {
+    protected door: boolean;
+    protected key: Key;
+    protected tenants: Person[] = [];
+    constructor(key: Key) {
+        this.door = false;
+        this.key = key;
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
